1711b4124092dc04dec28c5c674cd35269a2a628
Dat Nguyen <vespa9893@gmail.com>
2025-05-08 10:09:18 +0700

    Removed folder from tracking

--- dist/app/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const greeting_1 = require("./greeting");
-process.stdout.write(`${(0, greeting_1.greet)('Alice')}\n`);
